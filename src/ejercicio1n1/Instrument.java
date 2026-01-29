e2533a5b184e3be690c993ca34f90471d2042641
AlessandroPesole <alespesole@gmail.com>
2026-01-29 10:54:30 +0100

    Inicio ejercicio1n1, creación superclase Instrument

---
+++ src/ejercicio1n1/Instrument.java
@@ -0,0 +1,37 @@
+package ejercicio1n1;
+
+public abstract class Instrument {
+    private String name;
+    private double price;
+
+    public Instrument(String name, double price) {
+        this.name = verifyName(name);
+        this.price = verifyPrice(price);
+    }
+
+    public abstract void play();
+
+    public String toString() {
+        return "Instrument{" + this.name + ", "+ this.price + "$}";
+    }
+
+    public static void staticPlay() {
+        System.out.println("Instrument playing");
+    }
+
+    public static double verifyPrice(double price) {
+        if (price <= (double)0.0F) {
+            throw new IllegalArgumentException("Error: Price has to be positive");
+        } else {
+            return price;
+        }
+    }
+
+    public static String verifyName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Error: Name must not be null");
+        } else {
+            return name;
+        }
+    }
+}
